db16c4db9dbf14881314d0518a82070b28e90ef9
Pbgsa <pedrobgsa@gmail.com>
2025-10-02 22:07:32 -0300

    Some EP2 solutions

--- C++/Comp/EP2/sum_two.cpp
+++ C++/Comp/EP2/sum_two.cpp
@@ -11,34 +11,19 @@ int main() {
 
     int n, target_sum;
     cin >> n >> target_sum;
-    multimap<int, int> array;
-    for (int i = 0; i < n; i++) {
+
+    map<int, int> m;
+    for (int i = 1; i <= n; i++) {
         int num;
         cin >> num;
 
-        array.insert({num, i+1});
+        if (m.count(target_sum-num)){
+            cout << m[target_sum-num] << ' ' << i << '\n';
+            return 0;
+        } else {
+            m[num] = i;
+        }
     }
-
-    bool possible = false;
-    int ind1, ind2;
-    for (auto [num, indice] : array) {
-        int target = target_sum - num;
-
-        auto it = array.find(target);
-
-        printf("%d and %d\n", it->first, it->second);
-
-        // if (it != array.end() && it->first != num) {
-        //     possible = true;
-        //     ind1 = indice;
-        //     ind2 = array[target];
-        //     break;
-        // }
-    }
-
-    if (possible) {
-        cout << ind2 << ' ' << ind1 << '\n';
-    } else {
-        cout << "IMPOSSIBLE" << '\n';
-    }
+    cout << "IMPOSSIBLE" << '\n';
+    return 0;
 }
